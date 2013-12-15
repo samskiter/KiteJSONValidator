bcce22fd3501e2e4c1fb9922229cfb37be24734c
samskiter <samskiter@users.noreply.github.com>
2013-12-15 17:17:06 +0000

    initial commit

---
+++ KiteJSONValidator.h
@@ -0,0 +1,13 @@
+//
+//  KiteJSONValidator.h
+//  MCode
+//
+//  Created by Sam Duke on 15/12/2013.
+//  Copyright (c) 2013 Airsource Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KiteJSONValidator : NSObject
+
+@end
---
+++ KiteJSONValidator.m
@@ -0,0 +1,13 @@
+//
+//  KiteJSONValidator.m
+//  MCode
+//
+//  Created by Sam Duke on 15/12/2013.
+//  Copyright (c) 2013 Airsource Ltd. All rights reserved.
+//
+
+#import "KiteJSONValidator.h"
+
+@implementation KiteJSONValidator
+
+@end
